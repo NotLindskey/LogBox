06cd367bbff2e856b4fa645d5ea6008104cc38ac
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-04 12:30:57 -0600

    created log container component

---
+++ src/components/LogContainer/LogContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './LogContainer.css';
+
+function logContainer() {
+  return (
+    <div className="log-container">
+      <p>This is a log container!</p>
+    </div>
+  );
+}
+
+export default logContainer;
