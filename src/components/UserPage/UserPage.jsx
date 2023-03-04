f04e210ca246564dac6d4e24b9004d62c28e2106
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-03 19:25:50 -0600

    created "create new log" button

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const history = useHistory();
+
+  const createLogBtn = () => {
+    console.log('clicked!');
+    history.push('/createNewLog');
+  };
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
+
+      {/* lets user create log */}
+      <div>
+        <button onClick={createLogBtn}>create new log</button>
+      </div>
     </div>
   );
 }
