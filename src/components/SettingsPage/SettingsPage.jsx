6f8850bb0215ca196f733fe8bff28147da23b580
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-04 12:31:55 -0600

    created settingPage component containing username and password inputs and a cancel button

--- src/components/SettingsPage/SettingsPage.jsx
+++ src/components/SettingsPage/SettingsPage.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import CancelButton from '../CancelButton/CancelButton';
 
 function SettingsPage() {
+  const state = useState();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const updateChanges = () => {
+    console.table('update changes clicked!');
+  };
+
   return (
-    <div className="container">
-      <p>Settings Page</p>
+    <div>
+      <div>
+        <h1>Settings</h1>
+      </div>
+      <div>
+        {/* title input */}
+        <div>
+          <label htmlFor="Username">
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="Password">
+            Username:
+            <input
+              type="text"
+              name="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <button onClick={updateChanges}>Update Changes</button>
+        </div>
+        <div>
+          <CancelButton />
+        </div>
+      </div>
     </div>
   );
 }
