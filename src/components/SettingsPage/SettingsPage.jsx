4b0fa0f696dc76df8a66121882a0b51aa297b7e7
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-04 10:49:31 -0600

    created SettingsPage component

---
+++ src/components/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SettingsPage() {
+  return (
+    <div className="container">
+      <p>Settings Page</p>
+    </div>
+  );
+}
+
+export default SettingsPage;
