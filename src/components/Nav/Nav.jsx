116d1bceba25e73c96ba95ecd51c6345bb474c40
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-04 10:48:57 -0600

    Renamed Link to 'settings'

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -28,8 +28,8 @@ function Nav() {
               Home
             </Link>
 
-            <Link className="navLink" to="/info">
-              Info Page
+            <Link className="navLink" to="/settings">
+              Settings
             </Link>
 
             <LogOutButton className="navLink" />
