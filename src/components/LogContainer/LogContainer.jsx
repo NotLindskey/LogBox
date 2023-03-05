533572da4aa165443289634b9093905ddffbb644
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-05 17:42:14 -0600

    importing data using redux onto dom for individual log

--- src/components/LogContainer/LogContainer.jsx
+++ src/components/LogContainer/LogContainer.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './LogContainer.css';
+import LogStatsBar from '../LogStatsBar/LogStatsBar';
 
 function logContainer() {
   const dispatch = useDispatch();
 
-  // const user = useSelector((store) => store.user);
+  const log = useSelector((store) => store.log);
 
   useEffect(() => {
     dispatch({ type: 'FETCH_LOG' });
@@ -14,7 +15,15 @@ function logContainer() {
 
   return (
     <div className="log-container">
-      <p>logContainer</p>
+      {/* {JSON.stringify(logs)} */}
+      {log.map((log) => (
+        <div className="individual-logs" key={log.id}>
+          <p>Date: {log.date}</p>
+          <p>Start Time: {log.start_time}</p>
+          <p>End Time: {log.end_time}</p>
+          <p>Entry: {log.entry}</p>
+        </div>
+      ))}
     </div>
   );
 }
