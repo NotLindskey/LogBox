f9e357eac612838b441cb545763277503fa0dae8
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-05 14:13:02 -0600

    included dispatch

--- src/components/LogContainer/LogContainer.jsx
+++ src/components/LogContainer/LogContainer.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import './LogContainer.css';
 
 function logContainer() {
+  const dispatch = useDispatch();
+
+  // const user = useSelector((store) => store.user);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_LOG' });
+  }, [dispatch]);
+
   return (
     <div className="log-container">
-      <p>This is a log container!</p>
+      <p>logContainer</p>
     </div>
   );
 }
