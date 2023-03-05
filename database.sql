761871cf23817ebf7c539cdf5088edc2c8fad210
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-05 14:12:35 -0600

    added database tables

--- database.sql
+++ database.sql
@@ -3,8 +3,28 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- USER TABLE
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+
+-- LOG TABLE
+CREATE TABLE "log" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"date" date,
+	"start_time" time,
+	"end_time" time
+);
+
+ALTER TABLE "log"
+ADD COLUMN "entry" VARCHAR (2000);
+
+-- GAME TABLE
+CREATE TABLE "game" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR (1000)
 );
