a0bea82d57b10a87db78892a6b26d45bb8a04b28
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-06 23:12:32 -0600

    fixed form submit, payload fixed, removed two inputs

--- src/components/CreateLogForm/CreateLogForm.jsx
+++ src/components/CreateLogForm/CreateLogForm.jsx
@@ -1,107 +1,89 @@
 import React, { useState } from 'react';
-// import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-import CancelLogButton from '../CancelLogButton/CancelLogButton';
-
-// const entry = useSelector(store => store.entry)
+import './CreateLogForm.css';
 
 function createNewLog() {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const [title, setTitle] = useState('');
-  const [startTime, setStartTime] = useState('');
-  const [endTime, setEndTime] = useState('');
   const [date, setDate] = useState('');
   const [entry, setEntry] = useState('');
 
   const createLog = (event) => {
-    console.table('this is the form on click!');
+    alert('Log created!');
+    event.preventDefault();
+    console.table('this is form data'.createNewLog);
     dispatch({
       type: 'POST_LOG',
-      payload: title,
-      startTime,
-      endTime,
-      date,
-      entry,
+      payload: {
+        title: title,
+        date: date,
+        entry: entry,
+      },
     });
+    history.push('/home');
   };
 
   return (
-    <div>
-      <form onSubmit={createLog}>
-        <h1>Log New Entry</h1>
-        {/* title input */}
-        <div>
-          <label htmlFor="Title">
-            Title:
-            <input
-              type="text"
-              name="title"
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-            />
-          </label>
-        </div>
-        {/* start time input */}
-        <div>
-          <label htmlFor="Start Time">
-            Start Time:
-            <input
-              type="time"
-              name="start time"
-              value={startTime}
-              onChange={(event) => setStartTime(event.target.value)}
-            />
-          </label>
-        </div>
-        {/* end time input */}
-        <div>
-          <label htmlFor="End Time">
-            End Time:
-            <input
-              type="time"
-              name="end time"
-              value={endTime}
-              onChange={(event) => setEndTime(event.target.value)}
-            />
-          </label>
-        </div>
-        {/* date input */}
-        <div>
-          <label htmlFor="Date">
-            Date:
-            <input
-              type="date"
-              name="date"
-              value={date}
-              onChange={(event) => setDate(event.target.value)}
-            />
-          </label>
-        </div>
-        {/* entry input */}
-        <div>
-          <label htmlFor="Entry">
-            Entry:
-            <input
-              type="text"
-              name="entry"
-              value={entry}
-              onChange={(event) => setEntry(event.target.value)}
-            />
-          </label>
-        </div>
-        {/* Log Entry Button */}
-        <div>
-          <button>Create Entry</button>
-        </div>
-      </form>
+    <section>
       <div>
-        <div>
-          <CancelLogButton />
-        </div>
+        <h1>Log New Entry</h1>
       </div>
-    </div>
+
+      <div>
+        <form onSubmit={createLog}>
+          {/* title input */}
+          <div>
+            <label htmlFor="Title">
+              Title:
+              <input
+                type="text"
+                name="title"
+                value={title}
+                required
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </label>
+          </div>
+
+          {/* date input */}
+          <div>
+            <label htmlFor="Date">
+              Date:
+              <input
+                type="date"
+                name="date"
+                value={date}
+                required
+                onChange={(event) => setDate(event.target.value)}
+              />
+            </label>
+          </div>
+
+          {/* entry input */}
+          <div>
+            <label htmlFor="Entry">
+              Entry:
+              <input
+                type="text"
+                name="entry"
+                value={entry}
+                required
+                onChange={(event) => setEntry(event.target.value)}
+              />
+            </label>
+          </div>
+
+          {/* Log Entry Button */}
+          <div>
+            <button type="submit">Create Entry</button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
 
