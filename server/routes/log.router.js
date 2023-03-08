6720620d8dc57fcfc00222b28f6f6dfdb9038bbb
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-07 19:36:10 -0600

    added isAuthenticated, corrected syntax for post route

--- server/routes/log.router.js
+++ server/routes/log.router.js
@@ -1,44 +1,53 @@
 const express = require('express');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   // GET route code here
-  console.table('in the server GET Log!');
-  let queryText = 'SELECT * FROM "log" WHERE "user_id" = $1;';
-  pool
-    .query(queryText, [req.user.id])
-    .then((result) => {
-      console.table(result.rows);
-      res.send(result.rows);
-    })
-    .catch((error) => {
-      console.table('error with retrieving user logs');
-      res.sendStatus(500);
-    });
+  console.log('log server GET route');
+  if (req.isAuthenticated()) {
+    let queryText = 'SELECT * FROM "log" WHERE "user_id" = $1;';
+    pool
+      .query(queryText, [req.user.id])
+      .then((result) => {
+        console.table(result.rows);
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.table('error with retrieving user logs');
+        res.sendStatus(500);
+      });
+  }
 });
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
-  console.log('something');
-  const title = req.body.title;
-  const date = req.body.date;
-  const entry = req.body.entry;
-
-  const queryText = `INSERT INTO "log" (date, entry, title)
-  VALUES ($1, $2, $3)`;
-  pool
-    .query(queryText, [date, entry, title])
-    .then(() => res.sendStatus(201))
-    .catch((error) => {
-      console.table('unable to create entry at this time');
-      res.sendStatus(500);
-    });
+  if (req.isAuthenticated()) {
+    const queryText = `INSERT INTO "log" ("user_id", "date", "entry", "title")
+    VALUES ($1, $2, $3, $4)`;
+    pool
+      .query(queryText, [
+        req.user.id,
+        req.body.date,
+        req.body.entry,
+        req.body.title,
+      ])
+      .then((result) => {
+        console.log('new log created', result.row[0].id);
+      })
+      .catch((error) => {
+        console.table('unable to create entry at this time');
+        res.sendStatus(500);
+      });
+  }
 });
 
 module.exports = router;
