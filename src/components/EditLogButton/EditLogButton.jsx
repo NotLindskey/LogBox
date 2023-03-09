bc37a12e82b4e9a15edb42afb791a668c3458fa6
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-09 00:04:26 -0600

    created modal and close/cancel confirm button

--- src/components/EditLogButton/EditLogButton.jsx
+++ src/components/EditLogButton/EditLogButton.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import EditLogEntry from '../EditLogEntry/EditLogEntry';
 
 function editLogButton() {
   const history = useHistory();
+  const [showModal, setShowModal] = useState(false);
 
-  const editLogButton = () => {
-    history.push('/editLogEntry');
+  const closeModal = () => {
+    if (confirm('Data will not be saved, ok to cancel?')) {
+      setShowModal(false);
+    }
   };
+
   return (
     <div>
       <div>
-        <button onClick={editLogButton}>Edit Log</button>
+        <button onClick={() => setShowModal(true)}>Edit Log</button>
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <EditLogEntry />
+              <button onClick={closeModal}>Cancel</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
