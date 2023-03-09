0320838dbb07c0e80075b3b3d2bd87f5f0fa3532
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-09 15:30:41 -0600

    created a delete log button dispatch ** not working yet **

--- src/components/DeleteLogButton/DeleteLogButton.jsx
+++ src/components/DeleteLogButton/DeleteLogButton.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function deleteLogButton() {
   const dispatch = useDispatch();
+  const deleteLog = useSelector((store) => store.deleteLog);
+
+  const deleteLogButton = () => {
+    console.log('delete clicked');
+    if (confirm('did you want to delete log?')) {
+      dispatch({ type: 'DELETE_LOG' });
+    }
+  };
 
   return (
     <div>
