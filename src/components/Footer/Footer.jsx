39087f708ed06d8502226e836f6853e00b910b88
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-09 14:39:55 -0600

    updated footer copyright from prime to Linssi Colin Ruiz

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import './Footer.css';
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+// or even care what the redux state is, sos it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return <footer>&copy; Linssi Colin Ruiz</footer>;
 }
 
 export default Footer;
