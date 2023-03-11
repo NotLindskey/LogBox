846f4bdc0ea15cbaa757b84f48d8ed2400efc286
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-11 12:03:46 -0600

    working on delete route ** still not working **

--- server/routes/log.router.js
+++ server/routes/log.router.js
@@ -55,20 +55,20 @@ router.post('/', (req, res) => {
 // DELETE post route
 router.delete('/:id', rejectUnauthenticated, (req, res) => {
   console.table('in the delete log router', req.params.id); // check router
-  // if (req.isAuthenticated()) {
-  //   console.table('is authenticated'); // check authenticated
+  if (req.isAuthenticated()) {
+    console.table('is authenticated'); // check authenticated
 
-  //   const queryText = `DELETE from "log" WHERE user_id =$1;`;
-  //   pool
-  //     .query(queryText, [req.params.id])
-  //     .then((result) => {
-  //       res.send(result.rows);
-  //     })
-  //     .catch((error) => {
-  //       console.table('unable to delete log at this time', error);
-  //       res.sendStatus(500);
-  //     });
-  // }
+    const queryText = `DELETE from "log" WHERE user_id =$1;`;
+    pool
+      .query(queryText, [req.params.id])
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.table('unable to delete log at this time', error);
+        res.sendStatus(500);
+      });
+  }
 });
 
 module.exports = router;
