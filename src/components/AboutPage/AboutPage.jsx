5682c6a63d90d693e39e3b16ded0e32ea3c8aa05
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-16 20:04:06 -0500

    updated about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,13 +9,32 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <h2>About logBox!</h2>
-        <p>LogBox is an application customized to help you create logs.
-        </p>
+        <h2>About LogBox!</h2>
+        <p>Log as you go! </p>
+        LogBox will help you log your favorite stream highlights. Create logs on
+        your terms: daily, weekly, monthly, annually. Forget Excel sheets, notes
+        and journals. Keep all your logs in one place!
+        <p>Technologies used:</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Express</li>
+          <li>Postico</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Redux</li>
+          <li>Saga</li>
+          <li>Heroku</li>
+        </ul>
       </div>
+
+      <p>
+        Thank you to the Lydian Cohort and all of Prime staff and anyone who
+        supported me in this new journey.
+      </p>
     </div>
   );
 }
 
 export default AboutPage;
-
