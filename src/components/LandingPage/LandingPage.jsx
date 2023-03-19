8d82052f3ab83990f2b5280977813faceb0fb6ab
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-19 13:17:45 -0500

    html formatting and changed css

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,15 +2,12 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
-// CUSTOM COMPONENTS
-// import RegisterForm from '../RegisterForm/RegisterForm';
 import LoginPage from '../LoginForm/LoginForm';
 
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome to LogBox!');
   const history = useHistory();
 
-  // send user to registration
   const onStart = (event) => {
     history.push('/registration');
   };
@@ -19,26 +16,30 @@ function LandingPage() {
     <div className="container">
       <h2>{heading}</h2>
       <div className="grid">
-        <div>
-          <h1>Log as you go!</h1>
-          <h3>
+        <div className="grid-col grid-col_10">
+          <h1 className="heading-primary">Log as you go!</h1>
+          <h3 className="heading-secondary">
             LogBox will help you log your favorite highlights of just about
             anything!
           </h3>
-          <p>Create logs on your terms: daily, weekly, monthly, annually.</p>
-          <p>Forget Excel sheets, notes and journals.</p>
-          <p>Keep all your logs in one place!</p>
+          <ul className="features-list">
+            <li>
+              Create logs on your terms: daily, weekly, monthly, annually.
+            </li>
+            <li>Forget Excel sheets, notes and journals.</li>
+            <li>Keep all your logs in one place!</li>
+          </ul>
         </div>
 
         <div>
-          <h4>Not a member yet?</h4>
-          <button className="btn btn_sizeSm" onClick={onStart}>
+          <h4 className="heading-tertiary">Not a member yet?</h4>
+          <button className="btn btn_sizeSm btn_primary" onClick={onStart}>
             Start Here!
           </button>
         </div>
 
-        <div className="grid-col grid-col_8">
-          <h4>Already a Member?</h4>
+        <div className="grid-col grid-col_11">
+          <h4 className="heading-tertiary">Already a Member?</h4>
           <LoginPage />
         </div>
       </div>
