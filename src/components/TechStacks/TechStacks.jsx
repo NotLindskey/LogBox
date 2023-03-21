a15ab2590b9b7e208b186717f21264a130412fd3
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-21 08:17:59 -0500

    created component for TechStacks

---
+++ src/components/TechStacks/TechStacks.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function TechStacks() {
+  return (
+    <div>
+      <br />
+      <p>Technologies Used:</p>
+      <ul className="tech-stack">
+        <li>
+          <span>HTML</span>
+        </li>
+        <li>
+          <span>CSS</span>
+        </li>
+        <li>
+          <span>JavaScript</span>
+        </li>
+        <li>
+          <span>Express</span>
+        </li>
+        <li>
+          <span>Postico</span>
+        </li>
+        <li>
+          <span>React</span>
+        </li>
+        <li>
+          <span>Node.js</span>
+        </li>
+        <li>
+          <span>Redux</span>
+        </li>
+        <li>
+          <span>Saga</span>
+        </li>
+        <li>
+          <span>Heroku</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default TechStacks;
