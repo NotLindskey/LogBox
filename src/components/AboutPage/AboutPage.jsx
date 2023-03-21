ac42768a22efba879d090ecafd13dd62e2efc923
Linssi Colin Ruiz <linssi101@gmail.com>
2023-03-21 08:18:21 -0500

    imported TechStacks and created a modal

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,87 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import TechStacks from '../TechStacks/TechStacks';
 
 import './AboutPage.css';
 
 function AboutPage() {
+  const history = useHistory();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  // button that sends user back to theLandingPage
+  const sendToHomeHandle = () => {
+    history.push('/home');
+  };
+
+  // Render
   return (
     <div>
       <div>
         <div>
-          <div>
+          <div className="about-title">
             <h2>About LogBox!</h2>
           </div>
           <div className="container-wrapper">
             <div className="pink-board-container ">
-              <p>
-                LogBox is an innovative platform designed to help you keep track
-                of your favorite highlights of just about anything. Whether it's
-                sports, games, TV shows, books, or movies, LogBox makes it easy
-                for you to log and track your favorite moments.
-              </p>
+              <h4>
+                LogBox is an innovative platform designed to keep track of your
+                logs of just about anything.
+              </h4>
+              <ul>
+                <li>Sports</li>
+                <li>Games</li>
+                <li>TV Shows</li>
+                <li>Books</li>
+                <li>Movies</li>
+              </ul>
+              <h4> LogBox makes it easy for you!</h4>
             </div>
             <div className="blue-board-container ">
-              <p>
+              <h4>
                 Forget about the hassle of managing multiple Excel sheets,
-                notes, and journals - LogBox makes it simple to keep all your
-                logs in one convenient location.
+                notes, and journals
+              </h4>
+              <p>
+                - LogBox makes it simple to keep all your logs in one convenient
+                location.
               </p>
             </div>
             <div className="green-board-container ">
               <p>
-                Our platform is designed to help you stay organized and relive
-                your favorite moments anytime, anywhere. So why wait? Start
-                logging with LogBox today and experience the convenience of
-                having all your in one place.
+                Our platform's designed to help organize and relive your
+                favorite moments anytime.
               </p>
-            </div>
-            <div className="purple-board-container ">
               <p>
-                Thank you to the Lydian Cohort and all of Prime staff and anyone
-                who supported me in this new journey.
+                <em>
+                  <h4>So why wait?</h4>
+                </em>
+              </p>
+              <p>
+                Start logging with LogBox today and experience the convenience
+                of having all your logs in one place.
               </p>
             </div>
           </div>
         </div>
 
         <div>
-          <br />
-          <p>Technologies Used:</p>
-          <ul className="tech-stack">
-            <li>
-              <span>HTML</span>
-            </li>
-            <li>
-              <span>CSS</span>
-            </li>
-            <li>
-              <span>JavaScript</span>
-            </li>
-            <li>
-              <span>Express</span>
-            </li>
-            <li>
-              <span>Postico</span>
-            </li>
-            <li>
-              <span>React</span>
-            </li>
-            <li>
-              <span>Node.js</span>
-            </li>
-            <li>
-              <span>Redux</span>
-            </li>
-            <li>
-              <span>Saga</span>
-            </li>
-            <li>
-              <span>Heroku</span>
-            </li>
-          </ul>
+          <div>
+            <button onClick={handleButtonClick}>More Info</button>
+            {showModal && (
+              <div className="modal">
+                <div className="modal-content">
+                  <div className="purple-board-container ">
+                    <div className="purple-board-content"> </div>
+                    <h4>Thank you Lydian!</h4>
+                    <h4>
+                      To our instructor Casie Siekman and all of The Prime
+                      Academy Team!
+                    </h4>
+                    <h4>Family and Friends who understood and supported.</h4>
+                    <h4>The coworkers who motivated.</h4>
+                    <h4>
+                      Atlas!, <br />
+                      Thank you to all of for being here!
+                    </h4>
+                  </div>
+                  <div>
+                    <TechStacks />
+                  </div>
+
+                  <div>
+                    <img
+                      src="public/styling/imgIcons/lindskey6_emote.png"
+                      alt="img of Lindskey6 Emote"
+                    />
+                  </div>
+
+                  <div>
+                    <button onClick={handleCloseModal}>Close Info</button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <div>
-        <button>Home</button>
+        <div>
+          <button onClick={sendToHomeHandle}>Home</button>
+        </div>
       </div>
     </div>
   );
